fe3e9c1b6673a86a32f2f71e5eeceb242373b50c
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-11 22:55:03 -0500

    first commit

--- api/server.js
+++ api/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
-
+ 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
